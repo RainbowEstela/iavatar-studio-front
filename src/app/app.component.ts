803c2e423cc8dc8a422d2bdf3675f95d311ea3db
Estela <eloiseruiz15@gmail.com>
2024-05-11 11:13:24 +0200

    creado logo, preparando componentes basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutWebComponent } from './layouts/layout-web/layout-web.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LayoutWebComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layouts/layout-web/layout-web.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-web',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-web.component.html',
+  styleUrl: './layout-web.component.css'
+})
+export class LayoutWebComponent {
+
+}
