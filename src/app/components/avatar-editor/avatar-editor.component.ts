d9ad707e84d028ea40dccdb9565907dcd26709f7
Estela <eloiseruiz15@gmail.com>
2024-05-29 22:03:59 +0200

    menu de opciones y eleccion de genero

--- src/app/components/avatar-editor/avatar-editor.component.ts
+++ src/app/components/avatar-editor/avatar-editor.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import $ from 'jquery';
+import { Component, Input, OnInit } from '@angular/core';
+import $, { event } from 'jquery';
 
 @Component({
   selector: 'app-avatar-editor',
@@ -9,7 +9,47 @@ import $ from 'jquery';
   styleUrl: './avatar-editor.component.css'
 })
 export class AvatarEditorComponent implements OnInit {
+
+  @Input() id:Number = -1;
+
+  Prompt:String = "";
+
   ngOnInit(): void {
+
+    // esconder todos las las características
     $("img[class~='part']").hide();
+    $("div[class~='loading']").hide();
+    $("div[class~='elecc-des']").hide();
+
+    // inicializar eventos
+    $("div[class~='elecc']").on("click",this.eleccionMenu);
+    $("div[class~='gen-elecc-des']").on("click",this.eleccionGenero);
+
+    
   }
+
+  eleccionMenu(e:any) {
+    $("div[class~='elecc']").removeClass("opcion-activa")
+    $(e.target).addClass("opcion-activa")
+    var corresponde = $(e.target).attr("corresponde");
+
+    $("div[class~='elecc-des']").hide();
+    $("div[class~='"+ corresponde + "']").slideDown();
+
+    /* elegir por defecto */
+  }
+
+  eleccionGenero(e:any) {
+
+    $("div[class~='gen-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='gen-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='gen-elecc-des']").attr("imagen")
+
+    if(imagen) {
+      $("div[class~='torso'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+    }
+  }
+
 }
