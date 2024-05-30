afbd9cdf55a1ed90b8ecb19bfc3743e8e9c7c73e
Estela <eloiseruiz15@gmail.com>
2024-05-30 22:21:26 +0200

    editor terminado todo menos color de pelo y generacion de prompt

--- src/app/components/avatar-editor/avatar-editor.component.ts
+++ src/app/components/avatar-editor/avatar-editor.component.ts
@@ -16,7 +16,7 @@ export class AvatarEditorComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // esconder todos las las características
+    // esconder todos las las caracteristicas
     $("img[class~='part']").hide();
     $("div[class~='loading']").hide();
     $("div[class~='elecc-des']").hide();
@@ -24,7 +24,14 @@ export class AvatarEditorComponent implements OnInit {
     // inicializar eventos
     $("div[class~='elecc']").on("click",this.eleccionMenu);
     $("div[class~='gen-elecc-des']").on("click",this.eleccionGenero);
+    $("div[class~='car-elecc-des']").on("click",this.eleccionCara);
+    $("div[class~='pel-elecc-des']").on("click",this.eleccionPeinado);
+    $("div[class~='ojo-elecc-des']").on("click",this.eleccionOjos);
+    $("div[class~='nar-elecc-des']").on("click",this.eleccionNariz);
+    $("div[class~='boc-elecc-des']").on("click",this.eleccionBoca);
+    $("div[class~='ojo-color-elecc']").on("click",this.eleccionColorOjos);
 
+    // inicializar caracteristicas
     
   }
 
@@ -36,9 +43,10 @@ export class AvatarEditorComponent implements OnInit {
     $("div[class~='elecc-des']").hide();
     $("div[class~='"+ corresponde + "']").slideDown();
 
-    /* elegir por defecto */
   }
 
+
+  // cambia la eleccion de genero (menu)
   eleccionGenero(e:any) {
 
     $("div[class~='gen-elecc-des']").removeClass("elecc-activa");
@@ -51,5 +59,100 @@ export class AvatarEditorComponent implements OnInit {
       $("img[class~='" + imagen +"']").show();
     }
   }
+
+  // cambia la eleccion de cara (menu)
+  eleccionCara(e:any) {
+    $("div[class~='car-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='car-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='car-elecc-des']").attr("imagen")
+
+    if(imagen) {
+      $("div[class~='cabeza'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+    }
+  }
+
+  // cambia la eleccion del peinado (menu)
+  eleccionPeinado(e:any) {
+    $("div[class~='pel-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='pel-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='pel-elecc-des']").attr("imagen");
+    var imagenBack = $(e.target).closest("div[class~='pel-elecc-des']").attr("imagen-back");
+  
+
+    if(imagen) {
+      $("div[class~='peinados'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+
+      $("div[class~='fondo'] > img").hide();
+      if(imagenBack) {
+        $("img[class~='" + imagenBack +"']").show();
+      }
+    }
+  }
+
+  // cambia la eleccion de los ojos (menu)
+  eleccionOjos(e:any) {
+    $("div[class~='ojo-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='ojo-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='ojo-elecc-des']").attr("imagen")
+
+    if(imagen) {
+      $("div[class~='ojos'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+    }
+  }
+
+  // cambia la eleccion de la nariz (menu)
+  eleccionNariz(e:any) {
+    $("div[class~='nar-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='nar-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='nar-elecc-des']").attr("imagen")
+
+    if(imagen) {
+      $("div[class~='narices'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+    }
+  }
+
+  // cambia la eleccion de la boca (menu)
+  eleccionBoca(e:any) {
+    $("div[class~='boc-elecc-des']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='boc-elecc-des']").addClass("elecc-activa");
+
+    var imagen = $(e.target).closest("div[class~='boc-elecc-des']").attr("imagen")
+
+    if(imagen) {
+      $("div[class~='bocas'] > img").hide();
+      $("img[class~='" + imagen +"']").show();
+    }
+  }
+
+  // cambia la eleccion del color de los ojos (menu)
+  eleccionColorOjos(e:any) {
+    $("div[class~='ojo-color-elecc']").removeClass("elecc-activa");
+    $(e.target).closest("div[class~='ojo-color-elecc']").addClass("elecc-activa");
+
+    var color = $(e.target).closest("div[class~='ojo-color-elecc']").attr("color")
+    
+
+   
+    if(color) {
+      
+      $("div[class~='ojos'] > img").removeClass("negro");
+      $("div[class~='ojos'] > img").removeClass("marron");
+      $("div[class~='ojos'] > img").removeClass("rojo");
+      $("div[class~='ojos'] > img").removeClass("amarillo");
+      $("div[class~='ojos'] > img").removeClass("verde");
+      $("div[class~='ojos'] > img").removeClass("azul");
+      
+      $("div[class~='ojos'] > img").addClass(color);
+    }
+  }
+
 
 }
